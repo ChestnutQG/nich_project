8b57b520bc1707f924d77ec809001db7c6ceffe9
yizyin2 <yizyin@outlook.com>
2026-07-02 15:32:58 +0800

    fix: configure packaged mapper discovery

--- java/src/main/java/com/chuizhipu/shop/ShopApplication.java
+++ java/src/main/java/com/chuizhipu/shop/ShopApplication.java
@@ -4,11 +4,17 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 @MapperScan("com.chuizhipu.shop.mapper")
 public class ShopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShopApplication.class, args);
+        SpringApplication application = new SpringApplication(ShopApplication.class);
+        application.setDefaultProperties(Map.of(
+                "mybatis.mapper-locations", "classpath*:mapper/*.xml"
+        ));
+        application.run(args);
     }
 }
